de88b711b6126a94193a9d0935ec456c2b222bdb
RAO FAHAD <raofahad046@gmail.com>
2023-12-09 22:11:06 +0500

    created iam policies

--- lib/aws-pinpoint-email-service-stack.ts
+++ lib/aws-pinpoint-email-service-stack.ts
@@ -1,6 +1,7 @@
 import * as cdk from "aws-cdk-lib";
 import { Construct } from "constructs";
 import * as apigwv2 from "@aws-cdk/aws-apigatewayv2-alpha";
+import * as iam from "aws-cdk-lib/aws-iam";
 
 export class AwsPinpointEmailServiceStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
@@ -26,6 +27,35 @@ export class AwsPinpointEmailServiceStack extends cdk.Stack {
           allowOrigins: ["*"],
         },
       }
+    );
+
+    // ===============================================================================
+    // IAM: CREATED IAM POLICIES FOR PINPOINT AND KINESIS FIREHOSE STREAM
+    // ===============================================================================
+
+    const pinpoint_role = new iam.Role(
+      this,
+      `${service}-${stage}-pinpoint-role`,
+      {
+        assumedBy: new iam.CompositePrincipal(
+          new iam.ServicePrincipal("pinpoint.amazonaws.com"),
+          new iam.ServicePrincipal("lambda.amazonaws.com")
+        ),
+
+        managedPolicies: [
+          iam.ManagedPolicy.fromAwsManagedPolicyName(
+            "service-role/AWSLambdaBasicExecutionRole"
+          ),
+        ],
+      }
+    );
+
+    pinpoint_role.addToPolicy(
+      new iam.PolicyStatement({
+        effect: iam.Effect.ALLOW,
+        actions: ["mobiletargeting:SendMessages"],
+        resources: ["*"],
+      })
     );
   }
 }
